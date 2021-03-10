038cdcf59b2e5946d9448d23aaacbd51bf5e6e09
ni-k-azakov <nani.l.desu.l.ka@gmail.com>
2021-03-10 12:12:05 +0300

    :tada: Init: new project

---
+++ ThatDay/app/src/main/java/com/example/thatday/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.thatday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+fun calculatePeriod(view: View) {
+    
+}
